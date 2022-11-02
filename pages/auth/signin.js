bbe8c9dd6db13be2a50cc6508b80118e57c5161f
cassianpry <crmarinello@gmail.com>
2022-11-02 13:57:22 -0300

    add authentication and get user data from google

---
+++ pages/auth/signin.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { getProviders, signIn } from 'next-auth/react';
+
+import Header from '../../components/Header';
+
+export default function signin({ providers }) {
+  return (
+    <>
+      <Header />
+      <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="flex flex-col items-center">
+            <img
+              className="w-52 object-cover"
+              src="https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+              alt="google-logo"
+            />
+            <p className="text-sm italic my-10 text-center">
+              Este site foi criado para fins de aprendizagem.
+            </p>
+            <button
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-600"
+              onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+            >
+              Sign In with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
+}
