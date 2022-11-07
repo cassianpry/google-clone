34e3518e4cfdbfaa72c21692826ee3d63b4f9f3f
cassianpry <crmarinello@gmail.com>
2022-11-07 13:56:46 -0300

    complete result section, install html-react-parser and upgraded next-auth

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,3 +1,6 @@
+import Parser from 'html-react-parser';
+import Link from 'next/link';
+
 export default function SearchResults({ results }) {
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -5,6 +8,24 @@ export default function SearchResults({ results }) {
         About {results.searchInformation.formattedTotalResults} results (
         {results.searchInformation.formattedSearchTime} seconds)
       </p>
+      {results.items.map((res) => (
+        <div key={res.link} className="max-w-xl mb-8">
+          <div className="group">
+            <Link
+              className="group-hover:underline decoration-blue-800"
+              href={res.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-800">
+                {res.title}
+              </h2>
+            </Link>
+            <Link className="text-sm truncate" href={res.link}>
+              {res.formattedUrl}
+            </Link>
+          </div>
+          <p className="text-gray-600">{Parser(res.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 }
